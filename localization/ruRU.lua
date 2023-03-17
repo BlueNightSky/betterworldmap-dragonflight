3522faf4cef93ae665077cff925f6a68c8cc6495
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2023-03-17 06:59:37 +0300

    Update ruRU.lua

--- localization/ruRU.lua
+++ localization/ruRU.lua
@@ -6,16 +6,16 @@ if not L then return end
 ----------------------------------- GLOBAL ------------------------------------
 -------------------------------------------------------------------------------
 
-L['addon_description'] = nil
+L['addon_description'] = 'Настроить отображение значков для этой карты.'
 
 -------------------------------------------------------------------------------
 ----------------------------------- GROUPS ------------------------------------
 -------------------------------------------------------------------------------
 
 L['ancient_waygate_label'] = '{spell:386485}'
-L['aylaag_camp_label'] = 'лагерем Айлааг'
+L['aylaag_camp_label'] = 'лагерь Айлааг'
 L['elemental_storm_label'] = '{spell:392768}'
-L['froststone_vault_storm_label'] = 'Froststone Vault Primal Storm'
+L['froststone_vault_storm_label'] = 'Изначальная буря в Ледокаменном хранилище'
 L['grand_hunt_label'] = 'Великая охота'
 L['community_feast_label'] = '{spell:386441}'
 L['siege_on_dragonbane_keep_label'] = '{spell:388945}'
