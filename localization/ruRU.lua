c445c2f1f71ac4e9b94f2ff8c8927b6be33e38c2
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2023-03-17 07:11:12 +0300

    Update ruRU.lua

--- localization/ruRU.lua
+++ localization/ruRU.lua
@@ -1,6 +1,7 @@
 local ADDON_NAME, _ = ...
 local L = LibStub('AceLocale-3.0'):NewLocale(ADDON_NAME, 'ruRU')
 if not L then return end
+-- Russian translation by ZamestoTV
 
 -------------------------------------------------------------------------------
 ----------------------------------- GLOBAL ------------------------------------
