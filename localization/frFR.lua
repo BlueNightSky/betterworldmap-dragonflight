8499b05c1603062f1f1bc8dc743034817e033bc3
wyldclaw <88683851+wyldclaw@users.noreply.github.com>
2023-03-25 12:09:32 -0700

    Update frFR.lua
    Unify translation comments

--- localization/frFR.lua
+++ localization/frFR.lua
@@ -1,7 +1,7 @@
--- French translation by Lightuky EU-Khaz Modan
 local ADDON_NAME, _ = ...
 local L = LibStub('AceLocale-3.0'):NewLocale(ADDON_NAME, 'frFR')
 if not L then return end
+-- French translation by Lightuky EU-Khaz Modan
 
 -------------------------------------------------------------------------------
 ----------------------------------- GLOBAL ------------------------------------
