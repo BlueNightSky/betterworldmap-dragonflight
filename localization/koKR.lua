ae7558ba8812a603dff87961292110a63ed20af1
wyldclaw <88683851+wyldclaw@users.noreply.github.com>
2023-03-25 12:03:09 -0700

    Update koKR.lua
    Finish Korean localization

--- localization/koKR.lua
+++ localization/koKR.lua
@@ -6,7 +6,7 @@ if not L then return end
 ----------------------------------- GLOBAL ------------------------------------
 -------------------------------------------------------------------------------
 
-L['addon_description'] = 'Adjust icon display for this map.'
+L['addon_description'] = '이 지도의 아이콘 표시 조정.'
 
 -------------------------------------------------------------------------------
 ----------------------------------- GROUPS ------------------------------------
@@ -15,8 +15,8 @@ L['addon_description'] = 'Adjust icon display for this map.'
 L['ancient_waygate_label'] = '{spell:386485}'
 L['aylaag_camp_label'] = '아일라그 야영지'
 L['elemental_storm_label'] = '{spell:392768}'
-L['froststone_vault_storm_label'] = 'Froststone Vault Primal Storm'
-L['grand_hunt_label'] = 'Grand Hunt'
+L['froststone_vault_storm_label'] = '서리석 금고 원시 폭풍'
+L['grand_hunt_label'] = '사냥의 제전'
 L['community_feast_label'] = '{spell:386441}'
 L['siege_on_dragonbane_keep_label'] = '{spell:388945}'
 L['tuskarr_fishing_hole_label'] = '{spell:388968}'
