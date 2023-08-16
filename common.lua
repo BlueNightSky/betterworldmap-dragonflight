3834beeee004bfb062ef2444b52b3b03d69e42ca
wyldclaw <88683851+wyldclaw@users.noreply.github.com>
2023-08-15 23:33:57 -0700

    Add Kalimdor Cup, reorganize code, revert comments

--- common.lua
+++ common.lua
@@ -1,17 +1,21 @@
+-------------------------------------------------------------------------------
 ---------------------------------- NAMESPACE ----------------------------------
+-------------------------------------------------------------------------------
 local _, ns = ...
 local L = ns.locale
 
 local Group = ns.Group
+
+local Pin = ns.pin.Pin
 
 -------------------------------------------------------------------------------
 
 ns.expansion = 10 -- Dragonflight
 ns.parentMapID = 1978 -- Dragon Isles
 
-ns.map[ns.parentMapID] = {}
-
---------------------------------- DRAGON ISLE ---------------------------------
+-------------------------------------------------------------------------------
+----------------------------------- GROUPS ------------------------------------
+-------------------------------------------------------------------------------
 
 ns.groups.ANCIENT_WAYGATES = Group({
     atlas = 'flightmaster_ancientwaygate-taxinode_neutral',
@@ -140,61 +144,162 @@ ns.groups.RESEARCHERS_UNDER_FIRE = Group({
     name = 'researchers_under_fire'
 }) -- Researchers Under Fire
 
+----------------------------------- KALIMDOR ----------------------------------
+
+ns.groups.KALIMDOR_CUP = Group({
+    atlas = 'racing',
+    label = L['kalimdor_cup'],
+    name = 'kalimdor_cup'
+}) -- Kalimdor Cup
+
+-------------------------------------------------------------------------------
+------------------------------------ PINS -------------------------------------
+-------------------------------------------------------------------------------
+
+function AncientWaygate(attrs)
+    return Pin({
+        group = ns.groups.ANCIENT_WAYGATES,
+        coordinates = attrs.coordinates
+    })
+end -- Ancient Waygates
+
+ns.pin.AncientWaygate = AncientWaygate
+
+local AylaagCamp = Pin({
+    group = ns.groups.AYLAAG_CAMPS
+}) -- Aylaag Camp
+
+ns.pin.AylaagCamp = AylaagCamp
+
+local CommunityFeast = Pin({
+    group = ns.groups.COMMUNITY_FEAST
+}) -- Community Feast
+
+ns.pin.CommunityFeast = CommunityFeast
+
+local DragonbaneSiege = Pin({
+    group = ns.groups.DRAGONBANE_SIEGE
+}) -- Seige on Dragonbane Keep
+
+ns.pin.DragonbaneSiege = DragonbaneSiege
+
+local DragonridingRally = Pin({
+    group = ns.groups.DRAGONRIDING_RALLY
+}) -- Dragonriding Rally
+
+ns.pin.DragonridingRally = DragonridingRally
+
+local ElementalStorm = Pin({
+    group = ns.groups.ELEMENTAL_STORM
+}) -- Elemental Storm
+
+ns.pin.ElementalStorm = ElementalStorm
+
+local FishingHole = Pin({
+    group = ns.groups.FISHING_HOLE
+}) -- Fishing Hole
+
+ns.pin.FishingHole = FishingHole
+
+local FrostStoneVaultStorm = Pin({
+    group = ns.groups.FROSTSTONE_VAULT_STORM
+}) -- Frostone Vault Primal Storm
+
+ns.pin.FrostStoneVaultStorm = FrostStoneVaultStorm
+
+local GrandHunt = Pin({
+    group = ns.groups.GRAND_HUNTS
+}) -- Grand Hunt
+
+ns.pin.GrandHunt = GrandHunt
+
+local KalimdorCup = Pin({
+    group = ns.groups.KALIMDOR_CUP
+}) -- Kalimdor Cup
+
+ns.pin.KalimdorCup = KalimdorCup
+
+local ResearchersUnderFire = Pin({
+    group = ns.groups.RESEARCHERS_UNDER_FIRE,
+    coordinates = 87248274
+}) -- Researchers Under Fire
+
+ns.pin.ResearchersUnderFire = ResearchersUnderFire
+
+local ZaralekCavern = Pin({
+    group = ns.groups.ZARALEK_CAVERN_ENTRANCES,
+    passive = true
+}) -- Zaralek Cavern
+
+ns.pin.ZaralekCavern = ZaralekCavern
+
+-------------------------------------------------------------------------------
 --------------------------------- MENU GROUPS ---------------------------------
+-------------------------------------------------------------------------------
 
 ns.menuGroups = {
-    [1] = {
-        title = L['zone_dragon_isles'],
-        groups = {
-            [1] = ns.groups.ANCIENT_WAYGATES,
-            [2] = ns.groups.DRAGONRIDING_RALLY,
-            [3] = ns.groups.ELEMENTAL_STORM,
-            [4] = ns.groups.FISHING_HOLE,
-            [5] = ns.groups.GRAND_HUNTS,
-            [6] = ns.groups.ZARALEK_CAVERN_ENTRANCES
+    [1978] = { -- Dragon Isles
+        [1] = {
+            title = L['zone_dragon_isles'],
+            groups = {
+                [1] = ns.groups.ANCIENT_WAYGATES,
+                [2] = ns.groups.DRAGONRIDING_RALLY,
+                [3] = ns.groups.ELEMENTAL_STORM,
+                [4] = ns.groups.FISHING_HOLE,
+                [5] = ns.groups.GRAND_HUNTS,
+                [6] = ns.groups.ZARALEK_CAVERN_ENTRANCES
+            }
+        },
+        [2] = {
+            title = L['zone_the_waking_shores'],
+            groups = {
+                [1] = ns.groups.FACTION_EXPEDITION,
+                [2] = ns.groups.DRAGONBANE_SIEGE
+            }
+        },
+        [3] = {
+            title = L['zone_ohnahran_plains'],
+            groups = {
+                [1] = ns.groups.FACTION_CENTAUR,
+                [2] = ns.groups.AYLAAG_CAMPS
+            }
+        },
+        [4] = {
+            title = L['zone_azure_span'],
+            groups = {
+                [1] = ns.groups.FACTION_TUSKARR,
+                [2] = ns.groups.COMMUNITY_FEAST
+            }
+        },
+        [5] = {
+            title = L['zone_thaldraszus'],
+            groups = {
+                [1] = ns.groups.FACTION_VALDRAKKEN,
+                [2] = ns.groups.GREAT_VAULT,
+                [3] = ns.groups.STORMS_FURY,
+                [4] = ns.groups.TIME_RIFT
+            }
+        },
+        [6] = {
+            title = L['zone_the_forbidden_reach'],
+            groups = {
+                [1] = ns.groups.FROSTSTONE_VAULT_STORM
+            }
+        },
+        [7] = {
+            title = L['zone_zaralek_cavern'],
+            groups = {
+                [1] = ns.groups.FACTION_NIFFEN,
+                [2] = ns.groups.RESEARCHERS_UNDER_FIRE
+            }
         }
     },
-    [2] = {
-        title = L['zone_the_waking_shores'],
-        groups = {
-            [1] = ns.groups.FACTION_EXPEDITION,
-            [2] = ns.groups.DRAGONBANE_SIEGE
-        }
-    },
-    [3] = {
-        title = L['zone_ohnahran_plains'],
-        groups = {
-            [1] = ns.groups.FACTION_CENTAUR,
-            [2] = ns.groups.AYLAAG_CAMPS
-        }
-    },
-    [4] = {
-        title = L['zone_azure_span'],
-        groups = {
-            [1] = ns.groups.FACTION_TUSKARR,
-            [2] = ns.groups.COMMUNITY_FEAST
-        }
-    },
-    [5] = {
-        title = L['zone_thaldraszus'],
-        groups = {
-            [1] = ns.groups.FACTION_VALDRAKKEN,
-            [2] = ns.groups.GREAT_VAULT,
-            [3] = ns.groups.STORMS_FURY,
-            [4] = ns.groups.TIME_RIFT
-        }
-    },
-    [6] = {
-        title = L['zone_the_forbidden_reach'],
-        groups = {
-            [1] = ns.groups.FROSTSTONE_VAULT_STORM
-        }
-    },
-    [7] = {
-        title = L['zone_zaralek_cavern'],
-        groups = {
-            [1] = ns.groups.FACTION_NIFFEN,
-            [2] = ns.groups.RESEARCHERS_UNDER_FIRE
+    [12] = { -- Kalimdor
+        [1] = {
+            title = L['zone_kalimdor'],
+            groups = {
+                [1] = ns.groups.KALIMDOR_CUP
+            }
         }
     }
 }
