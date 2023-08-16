ff1e59b18f450ac231d693299a5e0316758b87e8
wyldclaw <88683851+wyldclaw@users.noreply.github.com>
2023-08-15 23:32:39 -0700

    Add Kalimdor Cup

---
+++ zones/dragonriding.lua
@@ -0,0 +1,51 @@
+-------------------------------------------------------------------------------
+---------------------------------- NAMESPACE ----------------------------------
+-------------------------------------------------------------------------------
+local _, ns = ...
+local Map = ns.Map
+
+local KalimdorCup = ns.pin.KalimdorCup
+
+-------------------------------------------------------------------------------
+
+local ashenvale = Map({id = 63, parent = 12})
+local azshara = Map({id = 76, parent = 12})
+local desolace = Map({id = 66, parent = 12})
+local durotar = Map({id = 1, parent = 12})
+local felwood = Map({id = 77, parent = 12})
+local feralas = Map({id = 69, parent = 12})
+local silithus = Map({id = 81, parent = 12})
+local southernbarrens = Map({id = 199, parent = 12})
+local stonetalonmontains = Map({id = 65, parent = 12})
+local thousandneedles = Map({id = 64, parent = 12})
+local ungorocrater = Map({id = 78, parent = 12})
+local winterspring = Map({id = 83, parent = 12})
+
+-------------------------------------------------------------------------------
+-------------------------------- KALIMDOR CUP ---------------------------------
+-------------------------------------------------------------------------------
+
+ashenvale.pins[7499] = KalimdorCup -- Ashenvale Ambit
+
+azshara.pins[7498] = KalimdorCup -- Rocketway Ride
+
+desolace.pins[7502] = KalimdorCup -- Desolace Drift
+
+durotar.pins[7500] = KalimdorCup -- Durotar Tour
+
+felwood.pins[7494] = KalimdorCup -- Fel Flyby
+
+feralas.pins[7506] = KalimdorCup -- Feralas Ruins Ramble
+
+silithus.pins[7507] = KalimdorCup -- Ahn'Qiraj Circuit
+
+southernbarrens.pins[7503] = KalimdorCup -- Barrens Divide Dive
+southernbarrens.pins[7504] = KalimdorCup -- Razorfen Roundabout
+
+stonetalonmontains.pins[7501] = KalimdorCup -- Webwinder Weave
+
+thousandneedles.pins[7505] = KalimdorCup -- Thousand Needles Thread
+
+ungorocrater.pins[7509] = KalimdorCup -- Un'Goro Crater Circuit
+
+winterspring.pins[7495] = KalimdorCup -- Winter Wander
