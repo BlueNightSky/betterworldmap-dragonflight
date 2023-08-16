3c1b5f14e06684cc1cdcb5d39dd72b7fe7b76f0c
wyldclaw <88683851+wyldclaw@users.noreply.github.com>
2023-08-15 23:54:09 -0700

    Add missing races

--- zones/dragonriding.lua
+++ zones/dragonriding.lua
@@ -14,10 +14,12 @@ local desolace = Map({id = 66, parent = 12})
 local durotar = Map({id = 1, parent = 12})
 local felwood = Map({id = 77, parent = 12})
 local feralas = Map({id = 69, parent = 12})
+local mounthyjal = Map({id = 198, parent = 12})
 local silithus = Map({id = 81, parent = 12})
 local southernbarrens = Map({id = 199, parent = 12})
 local stonetalonmontains = Map({id = 65, parent = 12})
 local thousandneedles = Map({id = 64, parent = 12})
+local uldum = Map({id = 249, parent = 12})
 local ungorocrater = Map({id = 78, parent = 12})
 local winterspring = Map({id = 83, parent = 12})
 
@@ -37,6 +39,9 @@ felwood.pins[7494] = KalimdorCup -- Fel Flyby
 
 feralas.pins[7506] = KalimdorCup -- Feralas Ruins Ramble
 
+mounthyjal.pins[7496] = KalimdorCup -- Nordrassil Spiral
+mounthyjal.pins[7497] = KalimdorCup -- Hyjal Hotfoot
+
 silithus.pins[7507] = KalimdorCup -- Ahn'Qiraj Circuit
 
 southernbarrens.pins[7503] = KalimdorCup -- Barrens Divide Dive
@@ -47,5 +52,7 @@ stonetalonmontains.pins[7501] = KalimdorCup -- Webwinder Weave
 thousandneedles.pins[7505] = KalimdorCup -- Thousand Needles Thread
 
 ungorocrater.pins[7509] = KalimdorCup -- Un'Goro Crater Circuit
+
+uldum.pins[7508] = KalimdorCup -- Uldum Tour
 
 winterspring.pins[7495] = KalimdorCup -- Winter Wander
